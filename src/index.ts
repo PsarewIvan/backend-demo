674480ce670803b660caed755df83f726f8005dd
Ivan Psarew <psarew.ivan@yandex.ru>
2023-08-02 10:37:51 +0600

    feat: change api

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api', (req: any, res: any) => {
+app.get('/', (req: any, res: any) => {
     res.send('Hello World!!!!!');
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-
-app.use(express.static('public'));
